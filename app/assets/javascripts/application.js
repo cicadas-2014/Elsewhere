988d9522696a7a14d2aa16f4595040b9003822ef
Jessica Tatham <jectatham@gmail.com>
2014-06-26 21:41:34 -0400

    Working to add jquery and highstack libraries

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,68 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+$(function () {
+
+    Highcharts.setOptions({
+        chart: {
+            backgroundColor: '#272822'
+        },
+        title: {
+            style: {
+                color: 'white'
+            }
+        },
+        legend: {
+            title: {
+                style: {
+                    color: 'silver'
+                }
+            }
+        },
+        colorAxis: {
+            minColor: '#373832',
+            maxColor: '#baFF55'
+        }
+
+    });
+
+    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=world-population-density.json&callback=?', function (data) {
+
+        // Initiate the chart
+        $('#container').highcharts('Map', {
+            
+            title : {
+                text : 'Set general theme options'
+            },
+
+            mapNavigation: {
+                enabled: true,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
+                }
+            },
+
+            colorAxis: {
+                min: 1,
+                max: 1000,
+                type: 'logarithmic'
+            },
+
+            legend: {
+                title: {
+                    text: 'Population density (/km²)'
+                }
+            },
+
+            series : [{
+                data : data,
+                mapData: Highcharts.maps['custom/world'],
+                joinBy: ['iso-a2', 'code'],
+                name: 'Population density',
+                tooltip: {
+                    valueSuffix: '/km²'
+                }
+            }]
+        });
+    });
+});
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
