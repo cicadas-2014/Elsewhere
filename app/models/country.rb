b7a5398666b5ad409859f3793ff57c5b9575e517
Dave Kerr <davek09@gmail.com>
2014-06-29 17:42:07 -0400

    created info cards on show page

--- app/models/country.rb
+++ app/models/country.rb
@@ -3,4 +3,31 @@ class Country < ActiveRecord::Base
 	has_many :images
 	
 	validates :name, presence: true, uniqueness: true
+
+	def political_rating
+		if self.political_stability
+			if self.political_stability < -1.0
+				"Highest Risk"
+			elsif self.political_stability < 0
+				"Moderate Risk"
+			elsif self.political_stability < 1
+				"Minor Risk"
+			else
+				"Lowest Risk"
+			end
+		end
+	end
+
+	def malaria?
+		if self.is_malaria_hotspot
+			"Warning: Malaria Hotspot!"
+		else
+			"Not a known Malaria risk zone."
+		end
+	end
+
+	def display_language
+		self.language ? self.language : "Not sure, but English will probably be OK."
+	end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -425,21 +425,21 @@ end
 flickr = Flickr.new(ENV["FLICKR_KEY"])
 
 
-# # Country.all.each do |country|	
-# # 	photos = flickr.photos_search(
-# # 		content_type: 1, 
-# # 		safe_search: 1, 
-# # 		tags: "#{country.name}, travel, beautiful",
-# # 		tag_mode: "all", 
-# # 		privacy_filter: 1, 
-# # 		sort: "interestingness-desc",
-# # 		media: "photos",
-# # 		)
+Country.all.each do |country|	
+	photos = flickr.photos_search(
+		content_type: 1, 
+		safe_search: 1, 
+		tags: "#{country.name}, travel, beautiful",
+		tag_mode: "all", 
+		privacy_filter: 1, 
+		sort: "interestingness-desc",
+		media: "photos",
+		)
 	
-# # 	photos[0..3].each do |photo|
-# # 		country.images << Image.create(url: photo.source)
-# # 	end
-# # end
+	photos[0..3].each do |photo|
+		country.images << Image.create(url: photo.source)
+	end
+end
 
 # ############THIS SEEDS POLITICAL RATING###########
 
